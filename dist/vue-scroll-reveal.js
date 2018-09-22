ce6faed972cbd77e5fb75f4cca031ea35cf975c5
tserkov <james.churchard@gmail.com>
2018-09-22 17:58:06 -0500

    #16 add IE11 transpile support

--- dist/vue-scroll-reveal.js
+++ dist/vue-scroll-reveal.js
@@ -3,10 +3,13 @@
 Object.defineProperty(exports, "__esModule", {
   value: true
 });
+
+var _extends = Object.assign || function (target) { for (var i = 1; i < arguments.length; i++) { var source = arguments[i]; for (var key in source) { if (Object.prototype.hasOwnProperty.call(source, key)) { target[key] = source[key]; } } } return target; };
+
 var sr = require('scrollreveal')();
 
 function generateOptions(defaultOptions, bindingValue, bindingModifiers) {
-  var options = Object.assign({}, defaultOptions, bindingValue);
+  var options = _extends({}, defaultOptions, bindingValue);
 
   if (bindingModifiers) {
     if (bindingModifiers.reset) {
